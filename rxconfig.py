2681d85afc6c00e8a8345a9defba83f7431c81c0
Antony Kiriinya <99920754+iamparody@users.noreply.github.com>
2025-09-16 18:47:44 +0300

    Update rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -1,11 +1,7 @@
-# import reflex as rx
-
-# config = rx.Config(app_name="app", plugins=[rx.plugins.TailwindV3Plugin()])
-
-
 import reflex as rx
 
 config = rx.Config(
     app_name="app",
     plugins=[rx.plugins.TailwindV3Plugin()],
+    disable_plugins=["reflex.plugins.sitemap.SitemapPlugin"],
 )
